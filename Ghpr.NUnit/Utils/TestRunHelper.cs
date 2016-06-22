6e7fb8f69558ad963f126ce11f89c2b7f4dde958
Evgeniy Kosjakov <42_@mail.ru>
2016-06-22 20:24:41 +0300

    new test data added

--- Ghpr.NUnit/Utils/TestRunHelper.cs
+++ Ghpr.NUnit/Utils/TestRunHelper.cs
@@ -8,22 +8,35 @@ namespace Ghpr.NUnit.Utils
 {
     public static class TestRunHelper
     {
-        public static ITestRun GetTestRun(XmlNode finishNode)
+        public static ITestRun GetTestRun(XmlNode testNode)
         {
-            var test = new TestRun();
             try
             {
-                var guid = finishNode.SelectSingleNode("properties/property[@name='TestGuid']")?.GetAttribute("value");
-                test.Name = finishNode.GetAttribute("name") ?? "";
-                test.FullName = finishNode.GetAttribute("fullname") ?? "";
-                test.Guid = guid != null ? Guid.Parse(guid) : Guid.Empty;
-                test.Result = finishNode.GetAttribute("result") ?? "Unknown";
+                var guid = testNode.SelectSingleNode("properties/property[@name='TestGuid']")?.GetAttribute("value");
+                var r = testNode.GetAttribute("result");
+                var l = testNode.GetAttribute("label");
+
+                var test = new TestRun
+                {
+                    Name = testNode.GetAttribute("name") ?? "",
+                    FullName = testNode.GetAttribute("fullname") ?? "",
+                    TestGuid = guid != null ? Guid.Parse(guid) : Guid.Empty,
+                    Result = r != null ? (l != null ? $"{r}: {l}" : r) : "Unknown",
+                    DateTimeStart = testNode.GetAttribute("start-time", default(DateTime)),
+                    DateTimeFinish = testNode.GetAttribute("end-time", default(DateTime)),
+                    TestDuration = testNode.GetAttribute("duration", 0.0),
+                    Output = testNode.SelectSingleNode(".//output")?.InnerText ?? "",
+                    TestMessage = testNode.SelectSingleNode(".//message")?.InnerText ?? "",
+                    TestStackTrace = testNode.SelectSingleNode(".//stack-trace")?.InnerText ?? ""
+                };
+
+                return test;
             }
             catch (Exception ex)
             {
                 Core.Utils.Log.Exception(ex, "Exception in GetTestRun");
+                return new TestRun();
             }
-            return test;
         }
     }
 }
