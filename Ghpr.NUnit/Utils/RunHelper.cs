ac01255dfa755a07a91fe7a0c13c2a4be7b0624d
Evgeniy Kosjakov <42_@mail.ru>
2017-12-16 21:14:54 +0300

    generate report from xml file

---
+++ Ghpr.NUnit/Utils/RunHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Ghpr.Core;
+using Ghpr.Core.Enums;
+using Ghpr.NUnit.Extensions;
+using NUnit;
+
+namespace Ghpr.NUnit.Utils
+{
+    public class RunHelper
+    {
+        public static void CreateReportFromFile(string path)
+        {
+            try
+            {
+                var reporter = new Reporter(TestingFramework.NUnit);
+                var xmlString = File.ReadAllText(path);
+                var xmlNode = XmlHelper.CreateXmlNode(xmlString);
+                var testCases = xmlNode.SelectNodes("test-case")?.Cast<XmlNode>();
+                if (testCases == null)
+                {
+                    return;
+                }
+                var testRuns = testCases.Select(TestRunHelper.GetTestRun).ToList();
+                reporter.GenerateFullReport(testRuns);
+            }
+            catch (Exception ex)
+            {
+                var log = new Core.Utils.Log(GhprEventListener.OutputPath);
+                log.Exception(ex, "Exception in CreateReportFromFile");
+            }
+        }
+    }
+}
