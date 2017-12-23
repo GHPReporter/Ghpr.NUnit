12e42a6149e3aa3d4b84f34f71d60ed7cd8f6d50
Evgeniy Kosjakov <42_@mail.ru>
2017-12-23 20:14:04 +0300

    fixed loading xml results

--- Ghpr.NUnit/Utils/GhprRunHelper.cs
+++ Ghpr.NUnit/Utils/GhprRunHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
+using System.Xml.Linq;
 using Ghpr.Core;
 using Ghpr.Core.Enums;
 using Ghpr.NUnit.Extensions;
@@ -18,8 +19,10 @@ namespace Ghpr.NUnit.Utils
             {
                 var reporter = new Reporter(TestingFramework.NUnit);
                 var xmlString = File.ReadAllText(path);
-                var xmlNode = XmlHelper.CreateXmlNode(xmlString);
-                var testCases = xmlNode.SelectNodes("test-case")?.Cast<XmlNode>();
+                var doc = new XmlDocument();
+                doc.LoadXml(xmlString);
+                XmlNode node = doc.DocumentElement;
+                var testCases = node?.SelectNodes(".//*/test-case")?.Cast<XmlNode>();
                 if (testCases == null)
                 {
                     return;
