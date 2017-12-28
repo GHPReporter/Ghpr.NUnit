b504565223d38b8a7cb0f259cd0b72dabb7f51d2
Evgeniy Kosjakov <42_@mail.ru>
2017-12-28 19:32:06 +0300

    new method added

--- Ghpr.NUnit/Utils/GhprNUnitRunHelper.cs
+++ Ghpr.NUnit/Utils/GhprNUnitRunHelper.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml;
 using Ghpr.Core;
 using Ghpr.Core.Enums;
+using Ghpr.Core.Interfaces;
 using Ghpr.NUnit.Extensions;
 
 namespace Ghpr.NUnit.Utils
@@ -15,22 +17,33 @@ namespace Ghpr.NUnit.Utils
             try
             {
                 var reporter = new Reporter(TestingFramework.NUnit);
-                var xmlString = File.ReadAllText(path);
-                var doc = new XmlDocument();
-                doc.LoadXml(xmlString);
-                XmlNode node = doc.DocumentElement;
-                var testCases = node?.SelectNodes(".//*/test-case")?.Cast<XmlNode>();
-                if (testCases == null)
-                {
-                    return;
-                }
-                var testRuns = testCases.Select(TestRunHelper.GetTestRun).ToList();
+                var testRuns = GetTestRunsListFromFile(path);
                 reporter.GenerateFullReport(testRuns);
             }
             catch (Exception ex)
             {
                 var log = new Core.Utils.Log(GhprEventListener.OutputPath);
                 log.Exception(ex, "Exception in CreateReportFromFile");
+            }
+        }
+
+        public static List<ITestRun> GetTestRunsListFromFile(string path)
+        {
+            try
+            {
+                var xmlString = File.ReadAllText(path);
+                var doc = new XmlDocument();
+                doc.LoadXml(xmlString);
+                XmlNode node = doc.DocumentElement;
+                var testCases = node?.SelectNodes(".//*/test-case")?.Cast<XmlNode>().ToList();
+                var list = testCases?.Select(TestRunHelper.GetTestRun).ToList() ?? new List<ITestRun>();
+                return list;
+            }
+            catch (Exception ex)
+            {
+                var log = new Core.Utils.Log(GhprEventListener.OutputPath);
+                log.Exception(ex, "Exception in GetTestRunsListFromFile");
+                return null;
             }
         }
     }
