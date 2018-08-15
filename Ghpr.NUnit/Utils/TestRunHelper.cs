083b47ffdb9bce1cffa26aed0942f9c341b21900
Evgeniy Kosjakov <42_@mail.ru>
2018-08-15 21:06:02 +0300

    small cleanup

--- Ghpr.NUnit/Utils/TestRunHelper.cs
+++ Ghpr.NUnit/Utils/TestRunHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Xml;
 using Ghpr.Core.Common;
 using Ghpr.Core.Extensions;
