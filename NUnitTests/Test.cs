9b60d45c49ea181f49d14643bd19b6a9ce99c987
Evgeniy Kosjakov <42_@mail.ru>
2019-10-25 11:56:20 +0300

    trying to reproduce the issue v2

---
+++ NUnitTests/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTests
+{
+    public class Test : TestBase
+    {
+        [Test(Description = "This is a failed test")]
+        public void TestWhichIsFailing()
+        {
+            Assert.Fail("Failed to see how the screenshot is taken");
+        }
+    }
+}
